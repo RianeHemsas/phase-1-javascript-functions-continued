5533d2db54cf48f60553a0af8b8c39c1eba3ee48
Hemsas Riane <rayan.hemsas@gmail.com>
2023-07-30 19:26:14 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function  saturdayFun (activity ='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+
+}
+// console.log( saturdayFun("bathe my dog"));
+
+function  mondayWork (activity ='go to the office'){
+    return `This Monday, I will ${activity}.`;
+
+}
+console.log( mondayWork("work from home"));
+
+function wrapAdjective(string ="*"){
+    return function innerFunction(argument="special"){
+        return `You are ${string}${argument}${string}!`;
+ 
+    }
+}
+console.log( wrapAdjective("||"));
